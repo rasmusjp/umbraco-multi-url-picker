2697424806cf51831337f1f7a56b6f60b0241821
Jeavon Leopold <jeavon@crumpled-dog.com>
2014-04-27 15:55:57 +0100

    Adding Value Converter and full VS.NET solution setup with TestSite

---
+++ RJP.MultiUrlPicker/Models/MultiUrls.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Umbraco.Core.Logging;
+
+namespace RJP.MultiUrlPicker.Models
+{
+    public class MultiUrls : List<Link>
+    {
+        private readonly string _propertyData;
+
+        public MultiUrls(string propertyData)
+        {
+            _propertyData = propertyData;
+
+            if (!string.IsNullOrEmpty(propertyData))
+            {
+                var relatedLinks = JsonConvert.DeserializeObject<JArray>(propertyData);
+
+                foreach (var item in relatedLinks)
+                {
+                    var newLink = new Link(item);
+                    if (!newLink.Deleted)
+                    {
+                        this.Add(new Link(item));
+                    }
+                    else
+                    {
+                        LogHelper.Warn<MultiUrls>(
+                            string.Format("MultiUrlPicker value converter skipped a link as the node is deleted (Id: {0}), ", newLink.Id));
+                    }
+                }
+            }
+        }
+
+        public string PropertyData
+        {
+            get
+            {
+                return _propertyData;
+            }
+        }
+
+        public bool Any()
+        {
+            return Enumerable.Any(this);          
+        }
+    }
+}
