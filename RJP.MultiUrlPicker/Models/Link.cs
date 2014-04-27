f7a0a71e55eda771420aae45af041c657b6a3160
Jeavon Leopold <jeavon@crumpled-dog.com>
2014-04-27 15:55:57 +0100

    Adding Value Converter and full VS.NET solution setup with TestSite

---
+++ RJP.MultiUrlPicker/Models/Link.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using umbraco;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace RJP.MultiUrlPicker.Models
+{
+
+    public class Link
+    {
+        private readonly JToken _linkItem;
+        private string _name;
+        private int? _id;
+        private string _url;
+        private string _target;
+        private bool? _deleted;
+
+        public Link(JToken linkItem)
+        {
+            _linkItem = linkItem;
+        }
+
+        private IPublishedContent ContentItem
+        {
+            get
+            {
+                if (this.Id == null) return null;
+                if (UmbracoContext.Current == null) return null;
+                var umbHelper = new UmbracoHelper(UmbracoContext.Current);
+                return umbHelper.TypedContent((int)this.Id);
+            }
+        }
+
+        public int? Id
+        {
+            get
+            {
+                if (_id == null)
+                {
+                    _id = _linkItem.Value<int?>("id");
+                }
+                return _id;         
+            } 
+
+        }
+
+        public string Name
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_name))
+                {
+                    _name = _linkItem.Value<string>("name");
+                }
+                return _name;
+            }
+        }
+
+        internal bool Deleted
+        {
+            get
+            {
+                if (_deleted == null)
+                {
+                    if (this.Id != null)
+                    {
+                        _deleted = ContentItem == null;
+                    }
+                    else
+                    {
+                        _deleted = false;
+                    }     
+                }
+                return (bool)_deleted;
+            }
+        }
+
+        public string Url
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_url))
+                {
+                    if (this.ContentItem != null)
+                    {
+                        _url = ContentItem.Url;
+                    }
+                    else
+                    {
+                        _url = _linkItem.Value<string>("url");
+                    }
+                }
+                return _url;
+            }
+        }
+
+        public string Target
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_target))
+                {
+                    _target = _linkItem.Value<string>("target");
+                }
+                return _target == string.Empty ? null : _target;
+            }
+        }
+    }
+}
---
+++ RJP.MultiUrlPicker/MultiUrlPickerValueConveter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RJP.MultiUrlPicker.Models;
+using Umbraco.Core;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Web;
+
+namespace RJP.MultiUrlPicker
+{
+    [PropertyValueType(typeof(MultiUrls))]
+    [PropertyValueCache(PropertyCacheValue.All, PropertyCacheLevel.Content)]
+    public class MultiUrlPickerValueConveter : PropertyValueConverterBase
+    {
+        public override bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return propertyType.PropertyEditorAlias.Equals("RJP.MultiUrlPicker");
+        }
+        public override object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            if (source == null) return null;
+            var sourceString = source.ToString();
+
+            return UmbracoContext.Current != null ? new MultiUrls(sourceString) : null;
+        }
+    }
+}
