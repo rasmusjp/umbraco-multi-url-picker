d0a0433cbb3fa7a2fe37c3daf464bdcfff144b99
Douglas Ludlow <dludlow@cityofboise.org>
2014-07-09 18:28:11 -0600

    Added karma config.

---
+++ src/RJP.MultiUrlPicker/test/app.conf.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module('umbraco', [
+    'umbraco.filters',
+    'umbraco.directives',
+    'umbraco.resources',
+    'umbraco.services',
+    'umbraco.mocks',
+    'umbraco.security',
+    'ngCookies'
+]);
